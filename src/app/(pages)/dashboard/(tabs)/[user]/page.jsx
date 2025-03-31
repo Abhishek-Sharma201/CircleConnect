700c8adbdafc154e26c6c5eb74b71009e9539c63
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-03-31 13:40:46 +0530

    single

--- src/app/(pages)/dashboard/(tabs)/[user]/page.jsx
+++ src/app/(pages)/dashboard/(tabs)/[user]/page.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+"use client";
 
-const page = async ({ params }) => {
-  const user = await Promise.resolve(params);
+import { useParams } from "next/navigation";
+import { apiURL } from "@/src/constants";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-  return <div>{JSON.stringify(user.user)}</div>;
+const Page = () => {
+  const params = useParams();
+  const userParam = params?.user;
+
+  const [data, setData] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      const res = await fetch(`${apiURL}/api/search/${userParam}`);
+      const json = await res.json();
+      if (!res.ok) {
+        throw new Error(json.message);
+      }
+      setData(json.user);
+      console.log(json.user);
+      toast.success(json.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (userParam) {
+      (async () => {
+        await fetchUser();
+      })();
+    }
+  }, [userParam]);
+
+  return <div>{userParam}</div>;
 };
 
-export default page;
+export default Page;
