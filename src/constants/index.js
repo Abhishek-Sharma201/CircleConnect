0ba6de97fd943ca17351aec640eef0e43eaa9582
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-08-28 18:07:25 +0530

    env-local

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const apiURL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const apiURL =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_SERVER_URL
+    : process.env.SERVER_URL;
